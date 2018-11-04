256d204ebce2f42225f6f25a42d2122d7f9e8e8c
F-DK <aki@fukaidesign.com>
2018-11-04 16:45:58 +0100

    Added header incl. header CSS

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from '../images/logo-designcode.svg'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="header">
+    <div className="header-nav">
+    <Link className="nav__link" to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+    <Link className="nav__link" to="/courses">Courses</Link>
+    <Link className="nav__link" to="/downloads">Downloads</Link>
+    <Link className="nav__link" to="/workshops">Workshops</Link>
+    <Link className="nav__link" to="/buy"><button className="link__button">Buy</button></Link>
     </div>
   </div>
 )
