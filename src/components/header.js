03af9acf35db89ccfe8be6ad39025f97125cb656
F-DK <aki@fukaidesign.com>
2018-11-10 23:29:59 +0100

    States n Events

--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,43 @@ import Link from 'gatsby-link'
 import logo from '../images/logo-designcode.svg'
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="header-nav">
-    <Link className="nav__link" to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-    <Link className="nav__link" to="/courses">Courses</Link>
-    <Link className="nav__link" to="/downloads">Downloads</Link>
-    <Link className="nav__link" to="/workshops">Workshops</Link>
-    <Link className="nav__link" to="/buy"><button className="link__button">Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 48) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'header header-scrolled' : 'header'}>
+        <div className="header-nav">
+          <Link className="nav__link" to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+          <Link className="nav__link" to="/courses">Courses</Link>
+          <Link className="nav__link" to="/downloads">Downloads</Link>
+          <Link className="nav__link" to="/workshops">Workshops</Link>
+          <Link className="nav__link" to="/buy"><button className="link__button">Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
